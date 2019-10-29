e8e4dea617e45d4a5369e06d7b3632b8b8cbe9ad
Antoine Lequeux <lequeuxa@gmail.com>
2019-10-29 16:49:24 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(title: 'Laundry')
+Task.create(completed: true, title: 'Shopping')
+Task.create(completed: true, title: 'Cooking')
